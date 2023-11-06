e2ac4a26f690cd6ba07443aa27ae122337ab9128
NiRovil <nicolas199851@hotmail.com>
2023-11-06 19:35:04 -0300

    Upcasting and Downcasting

--- heranca-polimorfismo/src/application/Program.java
+++ heranca-polimorfismo/src/application/Program.java
@@ -1,13 +1,35 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		BusinessAccount account = new BusinessAccount();
+		Account acc = new Account(100, "Nicolas", 0.0);
+		BusinessAccount bacc = new BusinessAccount(101, "Maria", 0.0, 500.0);
 		
+		//Upcasting
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(102, "Felipe", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(103, "Ana", 0.0, 0.01);
+		
+		//Downcasting
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.00);
+		
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(100.00);
+		}
+		
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 		
 	}
 
